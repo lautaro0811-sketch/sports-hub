c538f1e4f08ccc4f06df5f6af64478fcb01b1a12
Lautaro Esposito <lautaro0811@gmail.com>
2026-09-16 16:42:23 +0000

    feat(models): validar solapamiento de reservas en la misma cancha y horario
    - Agregar validación personalizada 'no_overlapping_reservations' en Reservation
    - Excluir reservas canceladas del chequeo de solapamiento
    - Implementar tests unitarios para validar creación, colisión y fechas pasadas

--- test/integration/api/v1/courts_test.rb
+++ test/integration/api/v1/courts_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class Api::V1::CourtsTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    
+
     @sport = Sport.find_or_create_by!(name: "Fútbol")
     @complex = SportsComplex.first || SportsComplex.create!(name: "Complejo Test", address: "Calle 123")
     @court = @complex.courts.first || @complex.courts.create!(name: "Cancha 1", surface_type: "Césped", sport: @sport, base_price: 1000)
@@ -14,14 +14,14 @@ class Api::V1::CourtsTest < ActionDispatch::IntegrationTest
 
   test "debe listar las canchas de un complejo" do
     get api_v1_sports_complex_courts_url(@complex), headers: @headers
-    
+
     assert_response :success
     assert_kind_of Array, response.parsed_body
   end
 
   test "debe mostrar los detalles de una cancha especifica" do
     get api_v1_sports_complex_court_url(@complex, @court), headers: @headers
-    
+
     assert_response :success
     assert_equal @court.id, response.parsed_body["id"]
     assert_equal @court.name, response.parsed_body["name"]
